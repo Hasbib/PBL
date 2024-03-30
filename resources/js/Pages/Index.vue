62fc9c06090966e686c40652a6db4e391d9c00f4
Hasbib <155308338+Hasbib@users.noreply.github.com>
2024-03-31 00:15:34 +0700

    Update Index.vue

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -289,16 +289,16 @@
 
 
    <!--=================================
-           FOOTER START
-       ==================================-->
-       <footer class="tf__footer mt_100">
-            <div class="text-center p-4" style="background-color: #191e24f5; color: white;">
-                Copyright ©2024 Tim Website OLINAS
-            </div>
-        </footer>
-       <!--=================================
-           FOOTER END
-       ==================================-->
+       FOOTER START
+   ==================================-->
+    <footer class="tf__footer mt_100">
+        <div class="text-center p-4" style="background-color: #191e24f5; color: white;">
+            Copyright ©2024 Tim Website OLINAS
+        </div>
+   </footer>
+   <!--=================================
+       FOOTER END
+   ==================================-->
 
 
    <!--=============================
