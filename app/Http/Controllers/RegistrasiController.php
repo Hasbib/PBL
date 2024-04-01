0fbd7fe4ca0237464fdf94466a278f3edbbdca44
iqbalfr2015 <165238862+iqbalfr2015@users.noreply.github.com>
2024-04-01 10:06:14 +0700

    Update RegistrasiController.php

--- app/Http/Controllers/RegistrasiController.php
+++ app/Http/Controllers/RegistrasiController.php
@@ -2,139 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Users; 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Models\Users;
+use Illuminate\Support\Facades\Hash;
 
-class RegistrasiController extends Controller 
+class RegisterController extends Controller
 {
-    // Index
-    public function index()
-    {
-        // Ambil data dari tabel registrasis
-        $registrasis = Users::orderBy('username', 'asc')->get();
-
-        // Buat respons JSON
-        return response()->json([
-            'success' => true,
-            'message' => 'List Data Registrasi',
-            'data' => $registrasis
-        ], 200);
-    }
-
-    // Show
-    public function show($id)
-    {
-        // Temukan registrasi berdasarkan ID
-        $registrasi = Users::findOrFail($id); 
-        // Buat respons JSON
-        return response()->json([
-            'success' => true,
-            'message' => 'Detail Data Registrasi',
-            'data' => $registrasi
-        ], 200);
-    }
-
-    // Store
     public function store(Request $request)
     {
-        // Set validasi
-        $validator = Validator::make($request->all(), [
-            'username' => 'required', 
-            'email' => 'required', 
-            'password' => 'required',
+
+        $input = $request->all();
+
+        Users ::create([
+            'name' => $input['name'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password'])
         ]);
 
-        // Respons kesalahan validasi
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        // Simpan ke database
-        $registrasi = Users::create([
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
+        return response()->json([
+            'status' => true,
+            'message' => "Registration Success"
         ]);
-
-        // Sukses menyimpan ke database
-        if ($registrasi) {
-            return response()->json([
-                'success' => true,
-                'message' => 'Registrasi berhasil',
-                'data' => $registrasi
-            ], 201);
-        }
-
-        // Gagal menyimpan ke database
-        return response()->json([
-            'success' => false,
-            'message' => 'Registrasi gagal',
-        ], 409);
-    }
-
-    // Update
-    public function update(Request $request, $id)
-    {
-        // Set validasi
-        $validator = Validator::make($request->all(), [
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-        ]);
-
-        // Respons kesalahan validasi
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        // Temukan registrasi berdasarkan ID
-        $registrasi = Users::findOrFail($id);
-
-        // Jika registrasi ditemukan
-        if ($registrasi) {
-            // Update registrasi
-            $registrasi->update([
-                'username' => $request->username,
-                'email' => $request->email,
-                'password' => bcrypt($request->password), // Updated to hash the password
-            ]);
-
-            return response()->json([
-                'success' => true,
-                'message' => 'Data registrasi berhasil diupdate',
-                'data' => $registrasi
-            ], 200);
-        }
-
-        // Data registrasi tidak ditemukan
-        return response()->json([
-            'success' => false,
-            'message' => 'Data tidak ditemukan',
-        ], 404);
-    }
-
-    // Destroy
-    public function destroy($id)
-    {
-        // Temukan registrasi berdasarkan ID
-        $registrasi = Users::findOrFail($id);
-
-        // Jika registrasi ditemukan
-        if ($registrasi) {
-            // Hapus registrasi
-            $registrasi->delete();
-
-            return response()->json([
-                'success' => true,
-                'message' => 'Data registrasi berhasil dihapus',
-            ], 200);
-        }
-
-        // Data registrasi tidak ditemukan
-        return response()->json([
-            'success' => false,
-            'message' => 'Data tidak ditemukan',
-        ], 404);
     }
 }
