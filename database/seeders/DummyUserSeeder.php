5d42531e8e565ba3e19ac92b0771d5ce5ea52cbb
iqbalfr2015 <165238862+iqbalfr2015@users.noreply.github.com>
2024-04-15 02:06:33 +0700

    Update DummyUserSeeder.php

--- database/seeders/DummyUserSeeder.php
+++ database/seeders/DummyUserSeeder.php
@@ -16,20 +16,32 @@ class DummyUserSeeder extends Seeder
         [
             'name'=>'mas Admin',
             'username'=>'Admin123',
+            'email'=>'olimpiadeitnas2024@gmail.com',
             'role'=>'admin',
-            'password'=> bcrypt('12345678')
+            'password'=> bcrypt('$22@TV7GTnXbqtYf'),
+            'instansi'=>'Universitas Sebelas Maret',
+            'kontak'=>'081278458353',
+            'nik'=>'1234567890001'
         ],
         [
             'name'=>'mas Petugas',
             'username'=>'Petugas123',
+            'email'=>'PetugasOlinas@gmail.com',
             'role'=>'petugas',
-            'password'=> bcrypt('12345678')
+            'password'=> bcrypt('crF$#hURtqcv'),
+            'instansi'=>'Universitas Sebelas Maret',
+            'kontak'=>'081278458354',
+            'nik'=>'1234567890002'
         ],
         [
             'name'=>'mas Juri',
             'username'=>'Juri123',
+            'email'=>'JuriOlinas@gmail.com',
             'role'=>'juri',
-            'password'=> bcrypt('12345678')
+            'password'=> bcrypt('cvtusnTY#$dq'),
+            'instansi'=>'Universitas Sebelas Maret',
+            'kontak'=>'081278458355',
+            'nik'=>'1234567890003'
         ],
 
     ];
